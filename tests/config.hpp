a471d4f23cf344c44bd5625b6fc85fb90a52fbe2
hitode221 <marina.dr98@mail.ru>
2016-12-18 14:24:10 +0300

    Update config.hpp

--- tests/config.hpp
+++ tests/config.hpp
@@ -27,8 +27,8 @@
 static std::map<std::string, std::string> options_with_proxy = 
 { 
 	{ "webdav_hostname", 	"https://webdav.yandex.ru" },
-	{ "webdav_login", 		"webdav.test.login" },
-	{ "webdav_password", 	"webdav.test.password" },
+	{ "webdav_login", 		"test-hitode" },
+	{ "webdav_password", 	"hitode221" },
 	{ "proxy_hostname", 	"{proxy_hostname}" },
 	{ "proxy_login", 		"{proxy_login}" },
 	{ "proxy_password", 	"{proxy_password}" }
@@ -37,8 +37,8 @@ static std::map<std::string, std::string> options_with_proxy =
 static std::map<std::string, std::string> options_without_proxy =
 {
 	{ "webdav_hostname", 	"https://webdav.yandex.ru" },
-	{ "webdav_login", 	"webdav.test.login" },
-	{ "webdav_password", 	"webdav.test.password" }
+	{ "webdav_login", 	"test-hitode" },
+	{ "webdav_password", 	"hitode-221" }
 };
 
 #ifdef WITH_PROXY
