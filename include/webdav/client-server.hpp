90cd49774a0099bdb49a7c6ccc7b41e4c73692bc
hitode221 <marina.dr98@mail.ru>
2016-12-18 17:07:21 +0300

    Update client-server.hpp

--- include/webdav/client-server.hpp
+++ include/webdav/client-server.hpp
@@ -24,7 +24,7 @@ std::string getHash(std::string name)
 {
 	int inlen;
 	FILE * input;
-	fopen_s(&input, name.c_str(), "rb");
+	input = fopen(name.c_str(), "rb");
 	SHA256_CTX handler;
 	unsigned char inbuf[BUFSIZE];
 	unsigned char digest[SHA256_DIGEST_LENGTH];
@@ -37,7 +37,7 @@ std::string getHash(std::string name)
 	SHA256_Final(digest, &handler);
 	auto file_hash = name + ".hash";
 	FILE * out;
-	fopen_s(&out, file_hash.c_str(), "wb");
+	out = fopen(file_hash.c_str(), "wb");
 	fwrite(digest, 1, SHA256_DIGEST_LENGTH, out);
 	fclose(out);
 	fclose(input);
@@ -47,7 +47,7 @@ std::string getHash(std::string name)
 std::string hash_to_string(std::string hash_file)
 {
 	FILE * input;
-	fopen_s(&input, hash_file.c_str(), "rb");
+	input = fopen(hash_file.c_str(), "rb");
 	char buffer[32];
 	fread(buffer, sizeof(char), 32, input);
 	std::stringstream ss;
@@ -59,8 +59,8 @@ std::string hash_to_string(std::string hash_file)
 void encrypt(std::string name, std::string new_name) {
 	int outlen, inlen;
 	FILE * input, *output;
-	fopen_s(&input, name.c_str(), "rb");
-	fopen_s(&output, new_name.c_str(), "wb");
+	input = fopen(name.c_str(), "rb");
+	output = fopen(new_name.c_str(), "wb");
 	unsigned char inbuf[BUFSIZE], outbuf[BUFSIZE];
 	unsigned char key[32] = "1234567890098765432112345678900";
 	unsigned char iv[8] = "1234567";
@@ -84,8 +84,8 @@ void encrypt(std::string name, std::string new_name) {
 void decrypt(std::string name, std::string new_name) {
 	int outlen, inlen;
 	FILE * input, *output;
-	fopen_s(&input, name.c_str(), "rb");
-	fopen_s(&output, new_name.c_str(), "wb");
+	input = fopen(name.c_str(), "rb");
+	output = fopen(new_name.c_str(), "wb");
 	unsigned char inbuf[BUFSIZE], outbuf[BUFSIZE];
 	unsigned char key[32] = "1234567890098765432112345678900";
 	unsigned char iv[8] = "1234567";
