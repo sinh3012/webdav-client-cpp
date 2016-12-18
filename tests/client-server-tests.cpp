6aa2a38ba726d277c351b4582fc57e809350a9db
hitode221 <marina.dr98@mail.ru>
2016-12-18 16:06:09 +0300

    Update client-server-tests.cpp

--- tests/client-server-tests.cpp
+++ tests/client-server-tests.cpp
@@ -1,1 +1,7 @@
 #include "client-server.hpp"
+
+SCENARIO("Hash", "[getHash]"){
+  getHash("testfile.txt");
+  getHash("same_testfile.txt");
+  REQUIRE(hash_to_string("testfile.txt.hash") == hash_to_string("same_testfile.txt.hash));
+}
