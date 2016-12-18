dc1c73bbd29f3ff3b60f6e8ef5da096743111619
hitode221 <marina.dr98@mail.ru>
2016-12-18 16:22:41 +0300

    Update client-server-tests.cpp

--- tests/client-server-tests.cpp
+++ tests/client-server-tests.cpp
@@ -5,3 +5,23 @@ SCENARIO("Hash", "[getHash]"){
   getHash("same_testfile.txt");
   REQUIRE(hash_to_string("testfile.txt.hash") == hash_to_string("same_testfile.txt.hash"));
 }
+
+SCENARIO("crypt", "[crypt]"){
+  encrypt("testfile.txt", "en_testfile.txt");
+  decrypt("en_testfile.txt", "de_testfile.txt");
+  std::ifstream expected("testfile.txt");
+	std::ifstream output("de_testfile.txt");
+	std::string first, second;
+	
+	bool flag = true;
+
+	while (expected || output) {
+		std::getline(expected, first);
+		std::getline(output, second);
+		if (first != second) {
+			flag = false;
+			break;
+		}
+	}
+  REQUIRE(flag);
+}
