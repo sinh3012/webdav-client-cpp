231b768e006d65b2fae9d77da35fab05098df3a9
Lair Sinh <assassin3012@mail.ru>
2016-12-19 12:05:35 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -4,14 +4,17 @@
 
 SCENARIO("upload", "[U]"){
 	UtoD();
-	/*REQUIRE(client->check("1.txt"));
-	REQUIRE(client->check("2.txt"));
-	REQUIRE(client->check("dir/"));
-	REQUIRE(client->check("dir/3.txt"));
-	REQUIRE(client->check("dir/dir_in/"));
-	REQUIRE(client->check("dir/dir_in/4.txt"));*/
+	REQUIRE(client->check("/path/f1.txt"));
+	REQUIRE(client->check("/path/1/"));
+	REQUIRE(client->check("/path/1/f2.txt"));
+	REQUIRE(client->check("/path/1/3/"));
+	REQUIRE(client->check("/path/1/3/f3.txt"));
+	REQUIRE(client->check("/path/1/3/f4.txt"));
+	REQUIRE(client->check("/path/2/"));
+	REQUIRE(client->check("/path/2/f2.txt"));
 }
 
-SCENARIO("download", "[D]"){
+/*SCENARIO("download", "[D]"){
 	DfromD();	
 }
+*/
