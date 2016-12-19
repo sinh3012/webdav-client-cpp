5a0f565e6c9a986414d09d253f11554d446b9d4f
Lair Sinh <assassin3012@mail.ru>
2016-12-19 10:56:00 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,51 +1,17 @@
-#include <webdav/server.hpp>
+#include <webdav/client.hpp>
 #include <webdav/Clie.hpp>
 #include "catch.hpp"
 
-SCENARIO("Hash", "[getHash]"){
-  getHash("testfile.txt");
-  getHash("same_testfile.txt");
-  REQUIRE(hash_to_string("testfile.txt.hash") == hash_to_string("same_testfile.txt.hash"));
-}
-
-SCENARIO("crypt", "[crypt]"){
-  encrypt("testfile.txt", "en_testfile.txt");
-  decrypt("en_testfile.txt", "de_testfile.txt");
-  std::ifstream expected("testfile.txt");
-	std::ifstream output("de_testfile.txt");
-	std::string first, second;
-	
-	bool flag = true;
-
-	while (expected || output) {
-		std::getline(expected, first);
-		std::getline(output, second);
-		if (first != second) {
-			flag = false;
-			break;
-		}
-	}
-  REQUIRE(flag);
-}
-
 SCENARIO("upload", "[upload]"){
-	std::unique_ptr<WebDAV::Client> client(WebDAV::Client::Init(init_client("config.txt")));
-	upload_to_disk_root("upload", client);
-	REQUIRE(client->check("1.txt"));
+	UtoDisk();
+	/*REQUIRE(client->check("1.txt"));
 	REQUIRE(client->check("2.txt"));
 	REQUIRE(client->check("dir/"));
 	REQUIRE(client->check("dir/3.txt"));
 	REQUIRE(client->check("dir/dir_in/"));
-	REQUIRE(client->check("dir/dir_in/4.txt"));
+	REQUIRE(client->check("dir/dir_in/4.txt"));*/
 }
 
 SCENARIO("download", "[download]"){
-	std::unique_ptr<WebDAV::Client> client(WebDAV::Client::Init(init_client("config.txt")));
-	download_from_disk_root("download", client);
-	REQUIRE(!client->check("1.txt"));
-	REQUIRE(!client->check("2.txt"));
-	REQUIRE(!client->check("dir/"));
-	REQUIRE(!client->check("dir/3.txt"));
-	REQUIRE(!client->check("dir/dir_in/"));
-	REQUIRE(!client->check("dir/dir_in/4.txt"));
+	DfromD();	
 }
