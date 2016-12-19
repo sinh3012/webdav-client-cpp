9cc734e400a37e9da4d85e65bffc3803cf9dc411
Lair Sinh <assassin3012@mail.ru>
2016-12-19 12:16:48 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -12,14 +12,14 @@ SCENARIO("upload", "[U]"){
 		{ "webdav_password", inf.password_.c_str() }
 	};
 	std::unique_ptr<WebDAV::Client> client(WebDAV::Client::Init(options));
-	REQUIRE(client->check("/path/f1.txt"));
+	REQUIRE(client->check("/path/f1.txt.aes"));
 	REQUIRE(client->check("/path/1/"));
-	REQUIRE(client->check("/path/1/f2.txt"));
+	REQUIRE(client->check("/path/1/f2.txt.aes"));
 	REQUIRE(client->check("/path/1/3/"));
-	REQUIRE(client->check("/path/1/3/f3.txt"));
-	REQUIRE(client->check("/path/1/3/f4.txt"));
+	REQUIRE(client->check("/path/1/3/f3.txt.aes"));
+	REQUIRE(client->check("/path/1/3/f4.txt.aes"));
 	REQUIRE(client->check("/path/2/"));
-	REQUIRE(client->check("/path/2/f2.txt"));
+	REQUIRE(client->check("/path/2/f2.txt.aes"));
 }
 
 /*SCENARIO("download", "[D]"){
